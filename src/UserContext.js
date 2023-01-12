38d7800e828a10c226001d0741ae389f9cbfa1f4
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-12 21:48:01 +0530

    user context

---
+++ src/UserContext.js
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+import Client from "webauthn/client";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const client = new Client({
+    pathPrefix: "/https://bewildered-pig-ring.cyclic.app/webauthn",
+  });
+
+  const register = async (username) => {
+    const res = await client.register({
+      username,
+      name: username,
+    });
+    console.log(res);
+  };
+
+  const login = () => {};
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        register,
+        login,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
