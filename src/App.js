6c6f52a9001ddc949ebbe58e0b641e433ef2e48a
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-13 04:46:20 +0530

    user provider

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Nav from "./Nav";
 import Register from "./Register";
+import { UserProvider } from "./UserContext";
 import WebAuthnEndorsement from "./WebAuthnEndorsement";
 
 function App() {
   const { pathName } = useLocation();
   return (
     <div className="App">
-      <Nav />
-      <AnimatePresence initial={false} mode="wait">
-        <Routes>
-          <Route path="/" key={pathName} element={<WebAuthnEndorsement />} />
-          <Route path="/register" key={pathName} element={<Register />} />
-        </Routes>
-      </AnimatePresence>
+      <UserProvider>
+        <Nav />
+        <AnimatePresence initial={false} mode="wait">
+          <Routes>
+            <Route path="/" key={pathName} element={<WebAuthnEndorsement />} />
+            <Route path="/register" key={pathName} element={<Register />} />
+          </Routes>
+        </AnimatePresence>
+      </UserProvider>
     </div>
   );
 }
