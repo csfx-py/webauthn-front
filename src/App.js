020bea052cc23d782041d8d605a9a69e7dc587d5
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-12 21:45:42 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { AnimatePresence } from "framer-motion";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Nav from "./Nav";
+import Register from "./Register";
 import WebAuthnEndorsement from "./WebAuthnEndorsement";
 
 function App() {
+  const { pathName } = useLocation();
   return (
     <div className="App">
       <Nav />
-      <WebAuthnEndorsement />
+      <AnimatePresence initial={false} mode="wait">
+        <Routes>
+          <Route path="/" key={pathName} element={<WebAuthnEndorsement />} />
+          <Route path="/register" key={pathName} element={<Register />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
